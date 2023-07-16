0bc14076158ad26cb2a9384686264075fd6c0f96
Favoursnazy4u <favoursnazy4u@gmail.com>
2023-07-16 10:28:16 +0100

    creating and init a new react native cli project

---
+++ src/components/auth/header/style.js
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+import {theme} from '../../../constants/styles';
+
+export default StyleSheet.create({
+  container: {
+    paddingVertical: 50,
+    backgroundColor: theme.colors.primary,
+    alignContent: 'center',
+    alignItems: 'center',
+  },
+});
---
+++ src/constants/styles.js
@@ -0,0 +1,7 @@
+export const theme = {
+  colors: {
+    primary: '#003153',
+    white: '#FFF',
+    black: '000',
+  },
+};
